2d9a7fea9f1ffa3e2471902edbd385f725fb6788
TamPhilip <philip.tam.code@gmail.com>
2018-12-23 18:39:59 -0500

    Added Comments

--- script.py
+++ script.py
@@ -1,10 +1,16 @@
 import pandas
-import matplotlib.pyplot as plt
+import matplotlib
+# reads the csv using pandas
 df = pandas.read_csv('data.csv')
+# selects the two columns that are needed
 df = df[['purpose','int_rate']]
+# groups the dataframe by the purpose and then gets the mean
 df = df.groupby('purpose').mean()
+# change the column name
 df.columns = ['avg_rate']
+# plot using values = average rate , rot (rotation of labels) set to 0 for horizontal
 df.plot.bar(y='avg_rate',rot=0)
-plt.show()
+# show using matplotlib
+matplotlib.pyplot.show()
 
 
